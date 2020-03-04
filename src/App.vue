c90010bc5cef9f4b79851a472748a2676eff779a
Sam Warner <its.sam.warner@gmail.com>
2020-03-04 14:04:59 -0500

    refactor(all): replace HelloWorld with Blink

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <blink><h1>text blinks here</h1></blink>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Blink from './components/Blink';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Blink
   }
 }
 </script>
---
+++ src/components/Blink.vue
@@ -0,0 +1,26 @@
+<template>
+  <span ref="blinkyText"> <slot /></span>
+</template>
+
+<script>
+export default {
+  name: 'blink',
+  mounted() {
+    setInterval(() => {
+      this.$refs.blinkyText.classList.toggle("onoff");
+    }, this.interval);
+  },
+  props: {
+    interval: {
+      type: Number,
+      default: 4000
+    }
+  }
+}
+</script>
+
+<style>
+.onoff {
+  visibility: hidden;
+}
+</style>
